397fcc7061530fbc503c6eae20e694b7fcee9e69
Earth <earthlono123@gmail.com>
2023-10-22 23:18:24 +1300

    Update user schema and add the post model

--- models/user.model.js
+++ models/user.model.js
@@ -1,11 +1,7 @@
 const mongoose = require("mongoose");
 
+
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    unique: true,
-    required: true,
-  },
   email: {
     type: String,
     unique: true,
@@ -16,6 +12,55 @@ const userSchema = new mongoose.Schema({
     minlength: 6,
     required: true,
   },
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  phone_number: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  contract: {
+    type: String,
+  },
+  skills: [],
+ 
+  coin: {
+    type: Number,
+  },
+  portfolio: {
+    id:{
+      type:String
+    },
+    img: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    skills: [String],
+    customer: {
+      type:String
+    }
+  },
+  posts:[{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:'post'
+  }]
 });
 
 module.exports = User = mongoose.model("user", userSchema);
